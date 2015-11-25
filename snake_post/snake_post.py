5fb34efc279a6a858dff14751a118792963963d0
Axel Fahy <axel@fahy.net>
2015-11-25 16:43:01 +0100

    beginning of  snakepost

--- snake_post/snake_post.py
+++ snake_post/snake_post.py
@@ -12,8 +12,24 @@ from snake_channel.snake_channel import SnakeChannel
 
 class SnakePost(SnakeChannel):
 
-    def __init__(self, channel):
+    def __init__(self, channel, udp=False):
         super(SnakePost, self).__init__(channel)
+        self.udp = udp
+        self.buffer = list() # List to store packets to send
         pass
+
+    def send_post(self, data, connection=(IP_SERVER, PORT_SERVER), ack=False):
+        if self.udp:    # on udp
+            print("UDP")
+            self.channel.sendto()
+        else:           # on snake_channel
+            print("Snake channel")
+
+    def receive_post(self):
+        if self.udp:    # on udp
+            print("UDP")
+            self.channel.recvfrom()
+        else:           # on snake_channel
+            print("Snake channel")
 
 
