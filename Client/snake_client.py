e8053679092cf1ccdd4c3c6f4ba1fa325c85100d
Rudolf Hohn <rudolf.hohn@etu.hesge.ch>
2015-12-23 16:25:53 +0100

    Ready OK, dev of body_p and players_info and snakes

--- Client/snake_client.py
+++ Client/snake_client.py
@@ -60,14 +60,7 @@ class Game(SnakePost):
         #self.nickname = self.preferences.get("nickname")
         self.f = Foods()
 
-        # Dictionary of players
-        # Key is the name of the player, value is a tuple
-        # of (color, score, state)
-        self.players_info = {}
-
-        # Dictionary of snakes
-        # Key is the name of the player, value is a list
-        # with positions
+        # Dict of Snake object
         self.snakes = {}
 
         # Score manager
@@ -129,20 +122,33 @@ class Game(SnakePost):
                         print "foods"
                     elif key == 'snakes':
                         for value in data_json[key]:
-                            self.snakes[value[0]] = value[1]
+                            self.snakes[value[0]].setBody(value[1])
                         print "snakes"
                     elif key == 'players_info':
-                        # Update name of player, colors and ready state of players
-                        self.players_info[data_json[key][0][0]] = [data_json[key][0][1], data_json[key][0][2], data_json[key][0][3]]
-                        # Update the score on gui
-                        print self.players_info
-                        for s in self.players_info:
-                            # If player already have a score, update it, else create a new score
-                            if self.players_info[s][Constants.D_SCORE]:
-                                self.scores.set_score(s, self.players_info[s][Constants.D_SCORE])
+
+                        # Find players that no longer exists
+                        for name in self.snakes:
+                            found = False
+                            for player_info in data_json[key]:
+                                if player_info[0] == name:
+                                    found = True
+
+                                if not found:
+                                    del self.snakes[name]
+                                    self.scores.del_score(name)
+
+                        # Parse the players_info
+                        for player_info in data_json[key]:
+
+                            # First time connection of a player
+                            if not self.snakes.get(player_info[0]):
+                                self.snakes[player_info[0]] = Snake(color=pygame.color.THECOLORS[player_info[1]], nickname=player_info[0])
+                                self.scores.new_score(player_info[0], self.snakes[player_info[0]].color)
+
+                            # Player is already connected, updating his scores
                             else:
-                                print self.players_info[s][Constants.D_COLOR]
-                                self.scores.new_score(s, pygame.color.THECOLORS[self.players_info[s][Constants.D_COLOR]], 0)
+                                self.scores.set_score(player_info[0], player_info[2])
+
                         print "players info"
                     elif key == 'game_over':
                         # Decrement score of player by 1
@@ -211,4 +217,4 @@ class Game(SnakePost):
 
 
 if __name__ == "__main__":
-    Game(Constants.IP_SERVER, Constants.PORT_SERVER, "green", "pasquier").run()
+    Game(Constants.IP_SERVER, Constants.PORT_SERVER, "red", "pasquier11").run()
